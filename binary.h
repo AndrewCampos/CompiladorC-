821e67fb138292a5da5b426135940eb1beca0f7c
AndrewCampos <a.campos98@live.com>
2020-09-01 18:43:26 -0300

    Sintese do codigo intermediario finalizada

--- binary.h
+++
@@ -1,8 +0,0 @@
-/****************************************************/
-/* Lab de Compiladores - Prof. Galvão               */
-/* File: binary.h                                   */
-/* The code generator implementation                */
-/* for the C- compiler                              */
-/****************************************************/
-
-void generateBinary (AssemblyCode a, int size);
