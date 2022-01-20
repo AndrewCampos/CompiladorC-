c8993633fbc02ef219ff53de5f2fbfe26686aa03
AndrewCampos <a.campos98@live.com>
2022-01-20 00:20:01 -0300

    Refactor: Código re-identado para facilitar leitura

--- source/assembly.h
+++ source/assembly.h
@@ -1,8 +1,21 @@
-typedef enum { formatR, formatI, formatSYS, format4, formatJ } InstrFormat;
+typedef enum {
+    formatR,
+    formatI,
+    formatSYS,
+    format4,
+    formatJ
+} InstrFormat;
 
-typedef enum { instr, lbl } LineKind;
+typedef enum {
+    instr,
+    lbl
+} LineKind;
 
-typedef enum { simple, vector, address } VarKind;
+typedef enum {
+    simple, // Variável simples
+    vector, // Vetor
+    address // Ponteiro para vetor
+} VarKind;
 
 typedef enum { $zero, $r1, $r2, $r3, $r4, $r5, $r6, $r7, $r8, $r9, $r10, $r11, $r12, $r13, $r14, $r15,
                 $r16, $r17, $r18, $r19, $p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9, $p10, $ret, $lp, none } Reg;
@@ -13,13 +26,13 @@ typedef enum {
 } InstrKind;
 
 typedef struct {
-    InstrFormat format;
-    InstrKind opcode;
+    InstrFormat format; // Formato da instrução
+    InstrKind opcode; // Código de identificação
     Reg reg1;
     Reg reg2;
     Reg reg3;
     int imed;
-    char * label;
+    char *label;
 } Instruction;
 
 typedef struct AssemblyCodeRec {
@@ -27,25 +40,25 @@ typedef struct AssemblyCodeRec {
     LineKind kind;
     union {
         Instruction instruction;
-        char * label;
+        char *label;
     } line;
-    struct AssemblyCodeRec * next;
-} * AssemblyCode;
+    struct AssemblyCodeRec *next;
+} *AssemblyCode;
 
 typedef struct VarListRec {
-    char * id;
+    char *id;
     int size;
     int memloc;
     VarKind kind;
-    struct VarListRec * next;
-} * VarList;
+    struct VarListRec *next;
+} *VarList;
 
 typedef struct FunListRec {
-    char * id;
+    char *id;
     int size;
     VarList vars;
-    struct FunListRec * next;
-} * FunList;
+    struct FunListRec *next;
+} *FunList;
 
 /**
  * @brief Gera código lista com as instruções do código assembly.
@@ -61,6 +74,6 @@ void generateAssembly(QuadList head);
  */
 AssemblyCode getAssembly();
 
-void printAssembly ();
+void printAssembly();
 
 int getSize();
